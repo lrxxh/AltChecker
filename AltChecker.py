620bc5175e838ff4b47d9d1201ec3534e4d8049f
lrxh <83221915+lrxxh@users.noreply.github.com>
2023-05-13 09:58:24 +0200

    Update AltChecker.py

--- AltChecker.py
+++ AltChecker.py
@@ -2,45 +2,43 @@ import os
 import json
 import requests
 
-# Path to the accounts.json file
+
 path = r"C:\Users\nodyt\.lunarclient\settings\game\accounts.json"
 
-# Check if the file exists
+
 if os.path.isfile(path):
-    # Read the contents of the file
+    
     with open(path) as f:
         data = json.load(f)
-        # Check if the "accounts" field exists in the JSON data
+        
         if "accounts" in data:
-            # Loop through each account in the "accounts" object
+
             for account in data["accounts"].values():
-                # Check if the account has a "username" field
+          
                 if "username" in account:
                     username = account["username"]
                     is_premium = "Premium" if not account.get("accessToken") else "Cracked"
                     print(f"{username} ({is_premium})")
         else:
             print("No Lunar Client accounts found in the file.")
-    # check if user added a Lunar Client account
+
     if "userAccount" in data:
         if "username" in data["userAccount"]:
             print("Added Lunar Client username:", data["userAccount"]["username"])
 else:
     print("Lunar Client file not found.")
 
-# Minecraft usernames from usercache.json
 user_name = os.environ['USERNAME']
 json_file_path = f'C:/Users/{user_name}/AppData/Roaming/.minecraft/usercache.json'
 
-# Check if the file exists
 if os.path.isfile(json_file_path):
     with open(json_file_path, 'r') as f:
         data = json.load(f)
 
-    printed_usernames = set()  # Set to store printed usernames
+    printed_usernames = set()  
     for item in data:
         username = item['name']
-        if username not in printed_usernames:  # Check if username has already been printed
+        if username not in printed_usernames:  
             is_premium = "Premium"
             try:
                 response = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{username}")
@@ -49,20 +47,19 @@ if os.path.isfile(json_file_path):
             except:
                 pass
             print(f"{username} ({is_premium})")
-            printed_usernames.add(username)  # Add the username to the set of printed usernames
+            printed_usernames.add(username)  
 else:
     print(f"{json_file_path} not found.")
 
-# Minecraft usernames from usernamecache.json
 username_cache_path = f'C:/Users/{user_name}/AppData/Roaming/.minecraft/usernamecache.json'
 
-# Check if the file exists
+
 if os.path.isfile(username_cache_path):
     with open(username_cache_path, 'r') as f:
         data = json.load(f)
 
     for username in data.values():
-        if username not in printed_usernames:  # Check if username has already been printed
+        if username not in printed_usernames:  
             is_premium = "Premium"
             try:
                 response = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{username}")
@@ -71,7 +68,7 @@ if os.path.isfile(username_cache_path):
             except:
                 pass
             print(f"{username} ({is_premium})")
-            printed_usernames.add(username)  # Add the username to the set of printed usernames
+            printed_usernames.add(username)  
 else:
     print(f"{username_cache_path} not found.")
 
