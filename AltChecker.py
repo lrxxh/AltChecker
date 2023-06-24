80e4e0db4a35f6e85194e137230db846a17c2f60
lrxh <83221915+lrxxh@users.noreply.github.com>
2023-06-24 10:59:32 +0200

    Update AltChecker.py

--- AltChecker.py
+++ AltChecker.py
@@ -3,11 +3,6 @@ import os
 import requests
 
 
-ACCOUNTS_FILE_PATH = os.path.join(os.getenv("USERPROFILE"), ".lunarclient", "settings", "game", "accounts.json")
-USERCACHE_FILE_PATH = os.path.join(os.getenv("APPDATA"), ".minecraft", "usercache.json")
-USERNAMECACHE_FILE_PATH = os.path.join(os.getenv("APPDATA"), ".minecraft", "usernamecache.json")
-
-
 def is_premium(username):
     try:
         response = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{username}")
@@ -21,23 +16,19 @@ def print_accounts(accounts):
     for account in accounts.values():
         if "username" in account:
             username = account["username"]
-            is_prem = "Premium" if not account.get("accessToken") else "Cracked"
+            is_prem = "Premium" if account.get("accessToken") else "Cracked"
             print(f"{username} ({is_prem})")
             printed_usernames.add(username)
     return printed_usernames
 
 
-def print_cache(json_file_path, printed_usernames, key="name"):
+def print_usercache(json_file_path, printed_usernames):
     if os.path.isfile(json_file_path):
         with open(json_file_path, 'r') as f:
-            try:
-                data = json.load(f)
-            except json.decoder.JSONDecodeError:
-                print(f"Failed to decode JSON data in {json_file_path}.")
-                return printed_usernames
+            data = json.load(f)
 
         for item in data:
-            username = item[key]
+            username = item['name']
             if username not in printed_usernames:
                 is_prem = is_premium(username)
                 print(f"{username} ({is_prem})")
@@ -47,9 +38,24 @@ def print_cache(json_file_path, printed_usernames, key="name"):
     return printed_usernames
 
 
+def print_usernamecache(username_cache_path, printed_usernames):
+    if os.path.isfile(username_cache_path):
+        with open(username_cache_path, 'r') as f:
+            data = json.load(f)
+
+        for user_id, username in data.items():
+            if username not in printed_usernames:
+                is_prem = is_premium(username)
+                print(f"{username} ({is_prem})")
+                printed_usernames.add(username)
+    else:
+        print(f"{username_cache_path} not found.")
+
+
 if __name__ == "__main__":
-    if os.path.isfile(ACCOUNTS_FILE_PATH):
-        with open(ACCOUNTS_FILE_PATH) as f:
+    path = os.path.join(os.getenv("USERPROFILE"), ".lunarclient", "settings", "game", "accounts.json")
+    if os.path.isfile(path):
+        with open(path) as f:
             data = json.load(f)
 
             if "accounts" in data:
@@ -59,14 +65,17 @@ if __name__ == "__main__":
                 printed_usernames = set()
 
         if "userAccount" in data and "username" in data["userAccount"]:
-            print(f"Added Lunar Client username: {data['userAccount']['username']}")
+            print("Added Lunar Client username:", data["userAccount"]["username"])
             printed_usernames.add(data["userAccount"]["username"])
     else:
         print("Lunar Client file not found.")
         printed_usernames = set()
 
-    printed_usernames = print_cache(USERCACHE_FILE_PATH, printed_usernames)
-    print_cache(USERNAMECACHE_FILE_PATH, printed_usernames)
+    user_name = os.getenv("USERNAME")
+    json_file_path = os.path.join(os.getenv("APPDATA"), ".minecraft", "usercache.json")
+    printed_usernames = print_usercache(json_file_path, printed_usernames)
+
+    username_cache_path = os.path.join(os.getenv("APPDATA"), ".minecraft", "usernamecache.json")
+    print_usernamecache(username_cache_path, printed_usernames)
 
     os.system("pause")
-# made by lrxh#0001
